7c81945efb711d2e8eb345bbea941669348e8f66
jrclegg <clegg.jonathan@rocketmail.com>
2017-06-18 12:51:36 +0100

    code moved from original working files

---
+++ App.js
@@ -0,0 +1,71 @@
+
+	var myApp = angular.module("PlayersApp", []);
+
+		myApp.controller("PlayersController", ["$scope", function($scope) {
+
+		// Define an empty array called scope.players
+			$scope.players = [];
+		
+		// Hide the create button at the 
+			$scope.show = false;
+
+		// Create a function to add players
+			$scope.addPlayer = function () {
+
+			// create an empty object to push into the array
+				var squadList = {};
+
+				squadList.name = $scope.playerName;
+				squadList.skill = $scope.playerSkill;
+				squadList.creativity = $scope.playerCreativity;
+				squadList.tackling = $scope.playerTackling;
+
+				$scope.players.push(squadList);
+
+				if ($scope.players.length >= 16) {
+
+					$scope.disabled = true;	
+		
+
+				} else {
+					$scope.disabled = false;
+
+				} 
+
+				if ($scope.players.length >=1) {
+
+					$scope.show = true;
+				}
+
+			} 
+
+			$scope.shuffle = function (array) {
+
+			//console.log(array);
+
+  			var currentIndex = array.length, temporaryValue, randomIndex;
+
+  			// While there remain elements to shuffle...
+ 			 while (0 !== currentIndex) {
+
+    		// Pick a remaining element...
+    		randomIndex = Math.floor(Math.random() * currentIndex);
+    		currentIndex -= 1;
+
+    		// And swap it with the current element.
+    		temporaryValue = array[currentIndex];
+    		array[currentIndex] = array[randomIndex];
+    		array[randomIndex] = temporaryValue;
+  			}
+
+  			// slice the array
+
+  			$scope.team1 = array.slice(0,5);
+  			$scope.team2 = array.slice(5,10);
+
+		}
+
+
+		}]);	
+
+			
